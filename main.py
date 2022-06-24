ffc62dfe0a012cec142bcbc12ad452aab1726ead
Vincenzo <vincenzo.pasquadibisceglie@miraclesrl.com>
2022-06-24 12:30:37 +0200

    main update

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from itertools import product
-from pm import Algo, Order, Miner, Smooth, DriftDiscoverMode
+from pm import Algo, Order, Miner,DriftDiscoverMode
 
-log_name = 'bpic2018Insp'
+log_name = 'RequestForPayment' #event log name
+init = 650# number of trace for the first model
 Miner.generate_csv(log_name)
-for algo, cut, top in product(Algo, [500], [None]):
-    print(f'{algo.name},{cut},{top}')
-    miner = Miner(log_name, Order.FRQ, algo, cut, top, update=True,
-                  drift_discover_algorithm=DriftDiscoverMode.ADWIN, no_sampling=True, summarization=True)
-    try:
-        miner.process_stream()
-        miner.save_results()
-        miner.generate_summary(log_name)
-    except Exception as e:
-        print(f'\tError: {e}')
+
+for a in Algo:
+    miner = Miner(
+        log_name=log_name,
+        order=Order.FRQ,
+        algo=a,
+        cut=init,
+        top=None,
+        update=True,
+        frequency=True,
+        filtering=True,
+        drift_discover_algorithm=DriftDiscoverMode.ADWIN,
+        summarization=False
+    )
+
+    miner.process_stream()
+    miner.save_results()
+    miner.generate_summary(log_name)

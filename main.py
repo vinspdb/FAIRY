107c5eabf3905310b98c7570686c13088864e6fb
vinspdb <57073656+vinspdb@users.noreply.github.com>
2024-11-11 11:17:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 from pm import Algo, Order, Miner,DriftDiscoverMode
-
-log_name = 'RequestForPayment' #event log name
-init = 650# number of trace for the first model
+import sys
+log_name = sys.argv[1]
+init = int(sys.argv[2])
+update = sys.argv[3]
+drift_mode = sys.argv[4]
+summ = sys.argv[5]
 Miner.generate_csv(log_name)
 
-for a in Algo:
-    miner = Miner(
+if update == 'True':
+    upd = True
+else:
+    upd= False
+
+if drift_mode == 'adwin':
+    drift = DriftDiscoverMode.ADWIN
+else:
+    drift = DriftDiscoverMode.NORMAL
+
+if summ == 'True':
+    gen_sum = True
+else:
+    gen_sum = False
+
+if alg == 'im':
+    disc_alg = Algo.IND
+elif alg == 'spl':
+    disc_alg = Algo.SPL
+elif alg == 'ilp':
+    disc_alg = Algo.ILP
+
+print('Configuration-->',log_name,init,update,drift)
+
+miner = Miner(
         log_name=log_name,
         order=Order.FRQ,
-        algo=a,
+        algo=disc_alg,
         cut=init,
         top=None,
-        update=True,
+        update=upd,
         frequency=True,
         filtering=True,
-        drift_discover_algorithm=DriftDiscoverMode.ADWIN,
-        summarization=False
+        drift_discover_algorithm=drift,
+        summarization=gen_sum
     )
-
-    miner.process_stream()
-    miner.save_results()
-    miner.generate_summary(log_name)
+miner.process_stream()
+miner.save_results()
+miner.generate_summary(log_name)

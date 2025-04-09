a9724b54b3790e98556796373266cc0de30d93ee
vinspdb <57073656+vinspdb@users.noreply.github.com>
2025-04-09 14:32:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ init = int(sys.argv[2])
 update = sys.argv[3]
 drift_mode = sys.argv[4]
 summ = sys.argv[5]
+method = sys.argv[6]
+result_name = sys.argv[7]
 Miner.generate_csv(log_name)
 
 if update == 'True':
@@ -12,37 +14,40 @@ if update == 'True':
 else:
     upd= False
 
-if drift_mode == 'adwin':
-    drift = DriftDiscoverMode.ADWIN
+if drift_mode == 'ADWIN':
+    print('drift ADWIN')
+    drift_mode = DriftDiscoverMode.ADWIN
+elif drift_mode == 'HDDM_W':
+    print('drift HDDM_W')
+    drift_mode = DriftDiscoverMode.HDDM_W
+elif drift_mode == 'EDDM':
+    print('drift EDDM')
+    drift_mode = DriftDiscoverMode.EDDM
 else:
-    drift = DriftDiscoverMode.NORMAL
+    drift_mode = DriftDiscoverMode.NORMAL
 
 if summ == 'True':
     gen_sum = True
 else:
     gen_sum = False
 
-if alg == 'im':
-    disc_alg = Algo.IND
-elif alg == 'spl':
-    disc_alg = Algo.SPL
-elif alg == 'ilp':
-    disc_alg = Algo.ILP
 
-print('Configuration-->',log_name,init,update,drift)
-
-miner = Miner(
+print('Configuration-->',log_name,init,update,drift_mode)
+for a in [Algo.IND]:
+    miner = Miner(
         log_name=log_name,
         order=Order.FRQ,
-        algo=disc_alg,
+        algo=a,
         cut=init,
         top=None,
+        method=method,
+        result_name = result_name,
         update=upd,
         frequency=True,
         filtering=True,
-        drift_discover_algorithm=drift,
+        drift_discover_algorithm=drift_mode,
         summarization=gen_sum
     )
-miner.process_stream()
-miner.save_results()
-miner.generate_summary(log_name)
+    miner.process_stream()
+    miner.save_results()
+    miner.generate_summary(log_name, result_name)
